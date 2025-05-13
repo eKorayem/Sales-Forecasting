337edada6635da91ae76986087239d669fcac3e9
Eslam Atia <eslam.mo.atia@gmail.com>
2025-05-14 00:12:41 +0300

    remove matplot importing

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
